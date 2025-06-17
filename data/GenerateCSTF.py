c1b9a70a2e16d73251072ec3e74155ee8dff5c62
barnden <brandon@nguyen.vc>
2025-06-17 00:28:16 -0500

    feat: Update example.cstf to reflect new bit format
    - 8-bit player fields
    - Updated player move/damage events
    - word addressing instead of dword for EventLUT

--- data/GenerateCSTF.py
+++ data/GenerateCSTF.py
@@ -9,7 +9,7 @@ CSTF_reserved = [0x0, 2]
 CSTF_version = [0x1, 1]
 CSTF_frame_rate = [0x40, 1]
 CSTF_map_name = b"de_mirage\0"
-CSTF_map_CRC = 0x12345678
+CSTF_build_info = 10462
 
 # GAME INFO
 steamids = [
@@ -32,7 +32,7 @@ usernames = [f"player {x}\0".encode("utf-8") for x in range(10)]
 
 # Round LUT
 CSTF_round_LUT = [
-    0b01_0000000010101010000000_000000000000000000000000,
+    0b01_000000_0010101010000000_000000000000000000000000,
 ] * 24
 
 # Insert a timeout after round 8
@@ -43,15 +43,15 @@ CSTF_round_LUT.insert(0, len(CSTF_round_LUT) * 6)
 print(f"CSTF_round_LUT: {CSTF_round_LUT[0]}")
 # Rounds
 CSTF_round_event_LUT = [
-    0b000001_000_000000000000010_000000000000000000000000,
-    0b000000_001_000000000000001_000000000000000000000001,
-    0b000000_001_000000000000110_000000000000000000000101,
-    0b000001_000_000000000000001_000000000000000000001001,
-    0b000001_000_000000000000001_000000000000000000001010,
-    0b000001_001_000000000000001_000000000000000000001011,
-    0b000001_011_000000000000001_000000000000000000001100,
-    0b000001_001_111111111111111_000000000000000000001101,
-    0b000000_101_111111111111111_000000000000000000001110,
+    0b000001_0000_000000000000000010_00000000000000000000, # 0
+    0b000000_0001_000000000000000001_00000000000000000011, # 3
+    0b000000_0001_000000000000000110_00000000000000001101, # 13
+    0b000001_0000_000000000000000001_00000000000000010111, # 23
+    0b000001_0000_000000000000000001_00000000000000011010, # 26
+    0b000001_0001_000000000000000001_00000000000000011101, # 29
+    0b000001_0011_000000000000000001_00000000000000100000, # 32
+    0b000001_0001_111111111111111111_00000000000000100011, # 35
+    0b000000_0101_111111111111111111_00000000000000100110, # 38
 ]
 
 # Insert the size of the LUT in bytes at beginning
@@ -61,68 +61,98 @@ CSTF_round_event_LUT.insert(0, len(CSTF_round_event_LUT) * 6)
 # Assuming type 0 is PlayerMoveEvent and type 1 is PlayerDamageEvent
 CSTF_round_event_data = [
     # DamageEvent: Player 0 damaged Player 1 (15HP, 7 Armor)
-    0b0000_0001_00001111_00000111_00000000,
+    0b00000000_00000001, # 0
+    0b00001111_00000111,
+    0b00000000_00000000,
     # MoveEvent: Player 2 set pitch to 27.99 deg and yaw to 275.98 deg
     #                         X: -1439.76, Y: -1322.04, Z: 11551.87
-    0b0010_0010011111010_110001000100001,
-    0b11000100101100111111100001010010,
-    0b11000100101001010100000101001000,
-    0b01000110001101000111111101111011,
+    # 0b0010_0010011111010_110001000100001,
+    0b10100000_00000000, # 3
+    0b1100010001000000,
+    0b1100111011011001,
+    0b00000010_00100111,
+    0b1111100001010010, # X
+    0b1100010010110011,
+    0b0100000101001000, # Y
+    0b1100010010100101,
+    0b0111111101111011, # Z
+    0b0100011000110100,
     # MoveEvent: Player 2 set pitch to 133.79 deg and yaw to 24.62 deg
     #                         X: -4759.16, Y: 1720.13, Z: 643.35
-    0b0010_1011111001001_000100011000001,
-    0b11000101100101001011100101001000,
-    0b01000100110101110000010000101001,
-    0b01000100001000001101011001100110,
+    0b11101111_00000000, # 13
+    0b0001000110000001,
+    0b0100011101110011,
+    0b00000010_10111110,
+    0b1011100101001000,
+    0b1100010110010100,
+    0b0000010000101001,
+    0b0100010011010111,
+    0b1101011001100110,
+    0b0100010000100000,
     # DamageEvent: Player 0 damaged Player 1 (15HP, 7 Armor)
-    0b0000_0001_00001111_00000111_00000000,
+    0b00000000_00000001, # 23
+    0b00001111_00000111,
+    0b00000000_00000000,
     # DamageEvent: Player 2 damaged Player 8 (3HP, 1 Armor)
-    0b0010_1000_00000011_00000001_00000000,
+    0b00000010_00001000, # 26
+    0b00000011_00000001,
+    0b00000000_00000000,
     # DamageEvent: Player 4 damaged Player 9 (86HP, 0 Armor)
-    0b0100_1001_01010110_00000000_00000000,
+    0b00000100_00001001, # 29
+    0b01010110_00000000,
+    0b00000000_00000000,
     # DamageEvent: Player 10 damaged Player 10 (5HP, 1 Armor)
-    0b1010_1010_00000101_00000001_00000000,
+    0b00001010_00001010, # 32
+    0b00000101_00000001,
+    0b00000000_00000000,
     # DamageEvent: Player 1 damaged Player 0 (15HP, 7 Armor)
-    0b0001_0000_00001111_00000111_00000000,
+    0b00000001_00000000, # 35
+    0b00001111_00000111,
+    0b00000000_00000000,
     # MoveEvent: Player 2 set pitch to 160.25 deg and yaw to 195.14 deg
     #                         X: 478.91, Y: -3480.56, Z: 355.27
-    0b0010_1110001111101_100010101100010,
-    0b01000011111011110111010001111011,
-    0b11000101010110011000100011110110,
-    0b01000011101100011010001010001111,
+    0b00100111_00000000, # yaw / padding (38)
+    0b1000101011000100, # yaw
+    0b1110100100111110, # pitch
+    0b00000010_11100011, # player / pitch (38)
+    0b0111010001111011,
+    0b0100001111101111, # X
+    0b1000100011110110,
+    0b1100010101011001, # Y
+    0b1010001010001111,
+    0b0100001110110001, # Z
 ]
 
 # I put these here since I'm reusing the same exact events for every round
 event_LUT_size = (len(CSTF_round_event_LUT) - 1) * 6
 event_LUT_padding = (event_LUT_size + 4) & 3
 
-rounds = 0
+round_count = 0
 for i in range(len(CSTF_round_LUT[1:])):
     if (CSTF_round_LUT[i] >> 46) != 1:
         continue
 
     CSTF_round_LUT[i] |= (
-        rounds
+        round_count
         * (
             4
             + len(CSTF_round_event_LUT[1:]) * 6
-            + len(CSTF_round_event_data) * 4
+            + len(CSTF_round_event_data) * 2
             + event_LUT_padding
         )
         // 4
     )
-    rounds += 1
+    round_count += 1
 
 CSTF_rounds = [
     to_bytes(4)(CSTF_round_event_LUT[0]),
     *map(to_bytes(6), CSTF_round_event_LUT[1:]),
     b"\x00" * event_LUT_padding,
-    *map(to_bytes(4), CSTF_round_event_data),
+    *map(to_bytes(2), CSTF_round_event_data),
 ]
 
 
 CSTF_rounds = b"".join(CSTF_rounds)
-print(CSTF_rounds)
 
 # END DATA
 
@@ -153,7 +183,7 @@ with open("example.cstf", "wb") as file:
     )
     file.write(CSTF_map_name)
 
-    file.write(to_bytes(4)(CSTF_map_CRC))
+    file.write(to_bytes(4)(CSTF_build_info))
 
     pad(file)
 
