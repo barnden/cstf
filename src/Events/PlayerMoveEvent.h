d1471a6723a3efd94c0660103f0a135b6b312858
barnden <brandon@nguyen.vc>
2025-06-18 00:35:08 -0500

    feat: update event pattern

--- src/Events/PlayerMoveEvent.h
+++ src/Events/PlayerMoveEvent.h
@@ -1,22 +1,27 @@
 #pragma once
 
+#include "BaseEvent.h"
 #include "Types.h"
 #include <format>
 
 namespace CSTF {
 
+struct PlayerMoveEvent : Event<PlayerMoveEvent> {
+    struct Data : public ISerializable<Data> {
+        // NOTE: Why do we get misalignment when casting char[80] to Data if we
+        //       pack the entire struct and not these fields.
+        //       sizeof(Data) is 20 in either case.
 #pragma pack(1)
-struct _PlayerMoveEventData {
-    u8 padding {};
-    u32 yaw : 24 {};
-    u32 pitch : 24 {};
-    u8 player {};
-};
+        u8 padding {};
+        u32 yaw : 24 {};
+        u32 pitch : 24 {};
+        u8 player {};
 #pragma pack()
 
-struct PlayerMoveEvent {
-    _PlayerMoveEventData data {};
-    float position[3] {};
+        float3 position;
+    } data {};
+
+    PlayerMoveEvent() = default;
 
     [[nodiscard]] constexpr auto pitch() const noexcept -> float
     {
@@ -28,13 +33,13 @@ struct PlayerMoveEvent {
         return static_cast<float>(data.yaw) / ((1ul << 24) - 1) * 360.;
     }
 
-    auto to_string() const -> std::string
+    [[nodiscard]] constexpr auto to_string() const noexcept -> std::string
     {
-        return std::format("PlayerMoveEvent(player: {}, pitch: {:0.3f}, yaw: {:0.3f}, position: {})",
+        return std::format("PlayerMoveEvent(player: {}, pitch: {:0.3f}, yaw: {:0.3f}, position: [{:0.3f}, {:0.3f}, {:0.3f}])",
                            data.player,
                            pitch(),
                            yaw(),
-                           position);
+                           data.position.x, data.position.y, data.position.z);
     }
 };
 
