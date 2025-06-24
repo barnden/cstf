047f030989decda930bc9089f1f5e01903d75a77
Brandon Nguyen <bgn@tamu.edu>
2025-06-24 01:50:53 -0500

    feat: assert that all `Event::Data` types are trivally copyable

--- src/Events/Events.h
+++ src/Events/Events.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <variant>
+#include <type_traits>
 
 #include "PlayerDamageEvent.h"
 #include "PlayerMoveEvent.h"
@@ -17,6 +18,8 @@ struct type_array {
     using get = std::tuple_element_t<I, tuple_t>;
 
     static constexpr size_t size = sizeof...(T);
+
+    static_assert(std::conjunction_v<std::is_trivially_copyable<typename T::Data>...>, "All event data must be trivially copyable.");
 };
 
 using EventTypes = type_array<
